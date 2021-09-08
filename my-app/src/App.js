518664d816cdfce5fc630bfb43e9fed7ac91d471
A Greaves <anna.j.greaves@gmail.com>
2021-09-08 10:00:18 +0000

    Add first functional component.

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreetng from "./components/FunctionalGreeting";
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreetng />
+    </div>
+  );
+}
+
+export default App;
